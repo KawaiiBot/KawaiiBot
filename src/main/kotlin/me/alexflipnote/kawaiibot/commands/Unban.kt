9eafe96ee3dc2f79f624e3ba4c353b2088fb6a70
Devoxin <lukel9914@gmail.com>
2018-06-11 21:25:57 +0100

    fix: Check if user is banned before attempting unban

--- src/main/kotlin/me/alexflipnote/kawaiibot/commands/Unban.kt
+++ src/main/kotlin/me/alexflipnote/kawaiibot/commands/Unban.kt
@@ -1,18 +1,28 @@
 package me.alexflipnote.kawaiibot.commands
 
+import kotlinx.coroutines.experimental.Unconfined
 import me.alexflipnote.kawaiibot.KawaiiBot
 import me.alexflipnote.kawaiibot.entities.Responses
 import me.aurieh.ichigo.core.CommandContext
+import me.aurieh.ichigo.core.CoroutineCommand
 import me.aurieh.ichigo.core.ICommand
 import me.aurieh.ichigo.core.annotations.Command
+import me.aurieh.ichigo.extensions.await
 import net.dv8tion.jda.core.Permission
 
 @Command(description = "Unbans a user from the current server.", guildOnly = true, userPermissions = [Permission.BAN_MEMBERS], botPermissions = [Permission.BAN_MEMBERS])
-class Unban : ICommand {
-    override fun run(ctx: CommandContext) {
+class Unban : CoroutineCommand(Unconfined) {
+    override suspend fun execute(ctx: CommandContext) {
         val member = ctx.args.nextSnowflakeOrNull() ?: return ctx.send("Give me someone to unban ;-;")
+        val bans = ctx.guild!!.banList.await()
+        val isUserBanned = bans.any { it.user.idLong == member }
+
+        if (!isUserBanned) {
+            return ctx.send("It looks like that user isn't banned...")
+        }
+
         val reason = ctx.args.collect()
-        ctx.guild!!.controller
+        ctx.guild.controller
                 .unban(member.toString())
                 .reason(Responses.responsible(ctx.author, reason))
                 .queue({
