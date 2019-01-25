a92106cb59e4c09e423aac90fec7b268fdecd5b6
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2019-01-25 19:26:01 +0100

    Changed URL due to broken API

--- src/main/kotlin/me/alexflipnote/kawaiibot/commands/Birb.kt
+++ src/main/kotlin/me/alexflipnote/kawaiibot/commands/Birb.kt
@@ -1,6 +1,7 @@
 package me.alexflipnote.kawaiibot.commands
 
 import me.alexflipnote.kawaiibot.extensions.closing
+import me.alexflipnote.kawaiibot.extensions.json
 import me.alexflipnote.kawaiibot.extensions.thenException
 import me.alexflipnote.kawaiibot.utils.RequestUtil
 import me.aurieh.ichigo.core.CommandContext
@@ -10,8 +11,9 @@ import me.aurieh.ichigo.core.annotations.Command
 @Command(description = "Cute birbs :3")
 class Birb : ICommand {
     override fun run(ctx: CommandContext) {
-        RequestUtil.get("https://random.birb.pw/tweet").thenAccept {
-            ctx.send("https://random.birb.pw/img/${it.closing()?.string()}")
+        RequestUtil.get("https://api.alexflipnote.xyz/birb").thenAccept {
+            val res = it.json()?.getString("file")
+            ctx.send("$res")
         }.thenException {
             ctx.send("I-I couldn't find any birbs... I'm sorry ;-;")
         }
