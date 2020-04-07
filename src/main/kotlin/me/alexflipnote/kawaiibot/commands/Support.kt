0ef45d96d83baf1f5685024cc0ea1998201c309a
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2020-04-07 13:43:43 +0200

    Update donate URL

--- src/main/kotlin/me/alexflipnote/kawaiibot/commands/Support.kt
+++ src/main/kotlin/me/alexflipnote/kawaiibot/commands/Support.kt
@@ -7,6 +7,6 @@ import me.aurieh.ichigo.core.annotations.Command
 @Command(description = "Posts a link to the official Patreon", aliases = ["patreon", "donate"])
 class Support : ICommand {
     override fun run(ctx: CommandContext) {
-        ctx.send("You can support me here \uD83C\uDF80\n**<https://www.patreon.com/KawaiiBot>**")
+        ctx.send("You can support me here \uD83C\uDF80\n**<https://github.com/sponsors/AlexFlipnote>**")
     }
 }
